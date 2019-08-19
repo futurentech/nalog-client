7a95157fc1706e1065f7543b449935606c4802a5
futurentech <futurentech@gmail.com>
2019-08-20 01:56:58 +0300

    test project

---
+++ src/nalog_client/driver.java
@@ -0,0 +1,47 @@
+package nalog_client;
+
+import unisoft.ws.*;
+import unisoft.ws.fnsndscaws2.request.NdsRequest2;
+import unisoft.ws.fnsndscaws2.request.ObjectFactory;
+import unisoft.ws.fnsndscaws2.response.NdsResponse2;
+import java.util.List;
+import java.util.Scanner;
+
+public class driver {
+    public static void main(String[] args) {
+        List<NdsRequest2.NP> request_list;
+        List<NdsResponse2.NP> response_list;
+        String result = null;
+
+        FNSNDSCAWS2Port service = new FNSNDSCAWS2().getFNSNDSCAWS2Port();
+        // create request
+        NdsRequest2 request = new ObjectFactory().createNdsRequest2();
+        NdsRequest2.NP request_np = new ObjectFactory().createNdsRequest2NP();
+        // create response
+        NdsResponse2 response = new unisoft.ws.fnsndscaws2.response.ObjectFactory().createNdsResponse2();
+        // user input
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Individual tax number: ");
+        String INN = in.nextLine();
+        System.out.print("Code of reason: ");
+        String KPP = in.nextLine();
+        System.out.print("Date in format(dd.mm.yy): ");
+        String DT = in.nextLine();
+
+        request_np.setINN(INN);
+        request_np.setKPP(KPP);
+        request_np.setDT(DT);
+
+        request_list = request.getNP();
+        request_list.add(request_np);
+
+        response = service.ndsRequest2(request);
+        response_list = response.getNP();
+
+        result = response_list.get(0).getState();
+        System.out.println(result);
+    }
+
+}
+
