19456654ffc66491a92fa0e0408d86346b5c1fa7
futurentech <futurentech@gmail.com>
2019-08-20 22:52:38 +0300

    commit 3

--- src/nalog_client/driver.java
+++ src/nalog_client/driver.java
@@ -15,24 +15,24 @@ public class driver {
     public static void main(String[] args) {
         List<NdsRequest2.NP> request_list;
         List<NdsResponse2.NP> response_list;
-        String result = null;
+        String result;
 
         FNSNDSCAWS2Port service = new FNSNDSCAWS2().getFNSNDSCAWS2Port();
         // create request
         NdsRequest2 request = new ObjectFactory().createNdsRequest2();
         NdsRequest2.NP request_np = new ObjectFactory().createNdsRequest2NP();
         // create response
-        NdsResponse2 response = new unisoft.ws.fnsndscaws2.response.ObjectFactory().createNdsResponse2();
+        NdsResponse2 response;
         // user input
 
         Scanner in = new Scanner(System.in);
-        System.out.print("Individual tax number: ");
+        System.out.print("INN: ");
         String INN = in.nextLine();
-        System.out.print("Code of reason: ");
+        System.out.print("KPP: ");
         String KPP = in.nextLine();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         String DT = dateFormat.format(new Date());
-        System.out.println("Date in format(dd.mm.yy): " + DT);
+        System.out.println("Date (dd.mm.yy): " + DT);
 
         request_np.setINN(INN);
         request_np.setKPP(KPP);
